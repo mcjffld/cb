9d1fcbd2883f9b7df725040bf0f2acdeeb56271b
Michael James <michael.james@priceline.com>
2014-12-20 22:07:23 -0500

    make open shift ready

--- server/app.js
+++ server/app.js
@@ -15,8 +15,11 @@ var server = require('http').createServer(app);
 require('./config/express')(app);
 require('./routes')(app);
 
+var serverPort = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var serverIP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
 // Start server
-server.listen(config.port, config.ip, function () {
+server.listen(serverPort, serverIP, function () {
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
 
