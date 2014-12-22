19e4d5112901a72f5f3966e5d30e6e29b31725df
Michael James <michael.james@priceline.com>
2014-12-21 23:25:35 -0500

    xx

--- server/config/express.js
+++ server/config/express.js
@@ -28,14 +28,14 @@ module.exports = function(app) {
   app.use(cookieParser());
   
   if ('production' === env) {
-    app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
-    app.use(express.static(path.join(config.root, 'public')));
-    app.set('appPath', config.root + '/public');
+//    app.use(favicon(path.join(config.root, 'favicon.ico')));
+//    app.use(express.static(path.join(config.root, 'public')));
+//    app.set('appPath', config.root);
     app.use(morgan('dev'));
   }
 
   if ('development' === env || 'test' === env) {
-    app.use(require('connect-livereload')());
+//    app.use(require('connect-livereload')());
     app.use(express.static(path.join(config.root, '.tmp')));
     app.use(express.static(path.join(config.root, 'client')));
     app.set('appPath', 'client');
--- server/routes.js
+++ server/routes.js
@@ -4,22 +4,14 @@
 
 'use strict';
 
-var express = require('express');
-
 var controller = require('./api/thing/thing.controller');
-
-var router = express.Router();
 
 module.exports = function(app) {
 
 
-  if (router) {
-    router.get('/api/things/', controller.index);
-    router.get('/api/things/up', controller.up);
-    router.get('/api/things/down', controller.down);
-  } else {
-    console.log ('no router ... ');
-  }
+  app.get('/api/things/', controller.index);
+  app.get('/api/things/up', controller.up);
+  app.get('/api/things/down', controller.down);
 
   // All other routes should redirect to the index.html
   app.get('*',function(req, res) {
