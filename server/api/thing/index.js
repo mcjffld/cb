d9fd6e43c348afa94b95601f502eda91c06037c2
Michael James <michael.james@priceline.com>
2014-12-21 22:57:31 -0500

    log

--- server/api/thing/index.js
+++ server/api/thing/index.js
@@ -1,11 +1,14 @@
 'use strict';
 
 var express = require('express');
+
+console.log (express);
+
 var controller = require('./thing.controller');
 
 var router = express.Router();
 
-//router.get('/', controller.index);
+router.get('/', controller.index);
 router.get('/up', controller.up);
 router.get('/down', controller.down);
 
