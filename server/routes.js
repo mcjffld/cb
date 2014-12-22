dd1c5b06a19d6a3ba686cbb07f5c9522e94e7841
Michael James <michael.james@priceline.com>
2014-12-21 23:18:32 -0500

    f

--- server/routes.js
+++ server/routes.js
@@ -4,8 +4,6 @@
 
 'use strict';
 
-var errors = require('./components/errors');
-
 var express = require('express');
 
 var controller = require('./api/thing/thing.controller');
@@ -23,13 +21,8 @@ module.exports = function(app) {
     console.log ('no router ... ');
   }
 
-  // All undefined asset or api routes should return a 404
-  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-   .get(errors[404]);
-
   // All other routes should redirect to the index.html
-  app.route('/*')
-    .get(function(req, res) {
+  app.get('*',function(req, res) {
       res.sendfile(app.get('appPath') + '/index.html');
     });
 };
